21c39349aec8002e973c35fb41ccc657e9f2a34d
ntkokd <natsunatsushokora@gmail.com>
2024-08-10 14:01:09 +0900

    game.tsの解読

--- client/src/scenes/Game.ts
+++ client/src/scenes/Game.ts
@@ -1,3 +1,5 @@
+//Phaser を使ってゲームシーンを管理するためのクラス Game を定義
+
 import Phaser from 'phaser'
 
 // import { debugDraw } from '../utils/debug'
@@ -23,6 +25,7 @@ import { setFocused, setShowChat } from '../stores/ChatStore'
 import { NavKeys, Keyboard } from '../../../types/KeyboardState'
 
 export default class Game extends Phaser.Scene {
+  //プロパティの定義
   network!: Network
   private cursors!: NavKeys
   private keyE!: Phaser.Input.Keyboard.Key
@@ -35,13 +38,14 @@ export default class Game extends Phaser.Scene {
   computerMap = new Map<string, Computer>()
   private whiteboardMap = new Map<string, Whiteboard>()
 
+  //このシーンに'game'というキーを割り当てる
   constructor() {
     super('game')
   }
 
   registerKeys() {
     this.cursors = {
-      ...this.input.keyboard.createCursorKeys(),
+      ...this.input.keyboard.createCursorKeys(),//'...'は展開構文（配列や要素を展開して挿入できる
       ...(this.input.keyboard.addKeys('W,S,A,D') as Keyboard),
     }
 
@@ -58,55 +62,63 @@ export default class Game extends Phaser.Scene {
     })
   }
 
-  disableKeys() {
+  disableKeys() {//キーの無効化
     this.input.keyboard.enabled = false
   }
 
-  enableKeys() {
+  enableKeys() {//キーの有効化
     this.input.keyboard.enabled = true
   }
 
   create(data: { network: Network }) {
+    //ネットワークの設定
     if (!data.network) {
       throw new Error('server instance missing')
     } else {
       this.network = data.network
     }
-
+    
+    //キャラクターのアニメーション
     createCharacterAnims(this.anims)
 
+    //マップの読み込み
     this.map = this.make.tilemap({ key: 'tilemap' })
     const FloorAndGround = this.map.addTilesetImage('FloorAndGround', 'tiles_wall')
 
+    //レイヤーの設定
     const groundLayer = this.map.createLayer('Ground', FloorAndGround)
-    groundLayer.setCollisionByProperty({ collides: true })
+    groundLayer.setCollisionByProperty({ collides: true })//コリジョンの設定
 
     // debugDraw(groundLayer, this)
 
-    this.myPlayer = this.add.myPlayer(705, 500, 'adam', this.network.mySessionId)
+    //プレイヤーの追加
+    this.myPlayer = this.add.myPlayer(705, 500, 'adam', this.network.mySessionId)//初期位置705,500にプレイヤーオブジェクトを追加
+    //プレイやオブジェクトの追加
     this.playerSelector = new PlayerSelector(this, 0, 0, 16, 16)
 
     // import chair objects from Tiled map to Phaser
-    const chairs = this.physics.add.staticGroup({ classType: Chair })
-    const chairLayer = this.map.getObjectLayer('Chair')
-    chairLayer.objects.forEach((chairObj) => {
-      const item = this.addObjectFromTiled(chairs, chairObj, 'chairs', 'chair') as Chair
+    const chairs = this.physics.add.staticGroup({ classType: Chair })//静的なオブジェクトを管理するグループを作成
+    const chairLayer = this.map.getObjectLayer('Chair')//Tiledマップから'Chair'という名前のレイヤーを取得
+    chairLayer.objects.forEach((chairObj) => {//chairLayer に含まれるすべてのオブジェクトを反復処理　chairObj は各椅子オブジェクトのデータ
+      const item = this.addObjectFromTiled(chairs, chairObj, 'chairs', 'chair') as Chair //Tiled マップのデータを元に、Phaser シーンに Chair オブジェクトを作成し、chairs グループに追加
       // custom properties[0] is the object direction specified in Tiled
-      item.itemDirection = chairObj.properties[0].value
+      item.itemDirection = chairObj.properties[0].value //Tiled マップで設定されたカスタムプロパティを使って、椅子オブジェクトの方向を設定
     })
 
     // import computers objects from Tiled map to Phaser
-    const computers = this.physics.add.staticGroup({ classType: Computer })
+    const computers = this.physics.add.staticGroup({ classType: Computer })//Computer オブジェクトを管理するための静的なグループを作成 のグループに追加されるオブジェクトはすべて Computer クラスのインスタンスとなる
     const computerLayer = this.map.getObjectLayer('Computer')
-    computerLayer.objects.forEach((obj, i) => {
+    computerLayer.objects.forEach((obj, i) => { //obj は各コンピュータオブジェクトのデータで、i はそのインデックス
       const item = this.addObjectFromTiled(computers, obj, 'computers', 'computer') as Computer
-      item.setDepth(item.y + item.height * 0.27)
+      item.setDepth(item.y + item.height * 0.27) //コンピュータオブジェクトが他のオブジェクトよりも前面に描画されるようにするため、深度を調整
+      //各 Computer オブジェクトに一意の ID を設定
       const id = `${i}`
       item.id = id
-      this.computerMap.set(id, item)
+      this.computerMap.set(id, item)//id をキーとして、item を computerMap に保存
     })
 
     // import whiteboards objects from Tiled map to Phaser
+    //chair,computerと同じwhiteboard版
     const whiteboards = this.physics.add.staticGroup({ classType: Whiteboard })
     const whiteboardLayer = this.map.getObjectLayer('Whiteboard')
     whiteboardLayer.objects.forEach((obj, i) => {
@@ -122,6 +134,7 @@ export default class Game extends Phaser.Scene {
     })
 
     // import vending machine objects from Tiled map to Phaser
+    // 自動販売機版
     const vendingMachines = this.physics.add.staticGroup({ classType: VendingMachine })
     const vendingMachineLayer = this.map.getObjectLayer('VendingMachine')
     vendingMachineLayer.objects.forEach((obj, i) => {
@@ -129,6 +142,8 @@ export default class Game extends Phaser.Scene {
     })
 
     // import other objects from Tiled map to Phaser
+    //Tiled マップで定義された他のオブジェクトを Phaser ゲームシーンにインポートするための処理
+    //trueのときオブジェクトは衝突する,falseのときオブジェクトは衝突しない
     this.addGroupFromTiled('Wall', 'tiles_wall', 'FloorAndGround', false)
     this.addGroupFromTiled('Objects', 'office', 'Modern_Office_Black_Shadow', false)
     this.addGroupFromTiled('ObjectsOnCollide', 'office', 'Modern_Office_Black_Shadow', true)
@@ -136,22 +151,26 @@ export default class Game extends Phaser.Scene {
     this.addGroupFromTiled('GenericObjectsOnCollide', 'generic', 'Generic', true)
     this.addGroupFromTiled('Basement', 'basement', 'Basement', true)
 
+    //他プレイヤーグループの作成
     this.otherPlayers = this.physics.add.group({ classType: OtherPlayer })
 
-    this.cameras.main.zoom = 1.5
-    this.cameras.main.startFollow(this.myPlayer, true)
+    this.cameras.main.zoom = 1.5 //カメラのズームレベル
+    this.cameras.main.startFollow(this.myPlayer, true) //カメラがプレイヤーを追従する
 
+    //this.myPlayer と this.myPlayer.playerContainer が groundLayer と vendingMachines と衝突するように設定(プレイヤーが地面や自動販売機とぶつかる)
     this.physics.add.collider([this.myPlayer, this.myPlayer.playerContainer], groundLayer)
     this.physics.add.collider([this.myPlayer, this.myPlayer.playerContainer], vendingMachines)
 
+    //オーバーラップの設定
+    //プレイヤーのカーソルなど(layerSelector)と対象オブジェクトが重なった時
     this.physics.add.overlap(
-      this.playerSelector,
-      [chairs, computers, whiteboards, vendingMachines],
-      this.handleItemSelectorOverlap,
-      undefined,
-      this
+      this.playerSelector,//オーバーラップの対象となるオブジェクト
+      [chairs, computers, whiteboards, vendingMachines],//オーバーラップの検出対象となるオブジェクト
+      this.handleItemSelectorOverlap,//オーバーラップが検出されたときに呼び出されるコールバック関数
+      undefined,//オーバーラップの発生条件を指定するためのオプションパラメータ(ここでは設定していない)
+      this //コールバック関数内で使用するコンテキストを指定(this='Game'シーン)
     )
-
+    //プレイヤーと他プレイヤーが重なった時
     this.physics.add.overlap(
       this.myPlayer,
       this.otherPlayers,
@@ -171,6 +190,7 @@ export default class Game extends Phaser.Scene {
     this.network.onChatMessageAdded(this.handleChatMessageAdded, this)
   }
 
+  //playerSelector（プレイヤーが現在選択しているアイテムを管理するオブジェクト）と selectionItem（プレイヤーが現在重なっているアイテム）との重なり処理
   private handleItemSelectorOverlap(playerSelector, selectionItem) {
     const currentItem = playerSelector.selectedItem as Item
     // currentItem is undefined if nothing was perviously selected
@@ -184,31 +204,37 @@ export default class Game extends Phaser.Scene {
     }
 
     // set selected item and set up new dialog
-    playerSelector.selectedItem = selectionItem
-    selectionItem.onOverlapDialog()
+    playerSelector.selectedItem = selectionItem //選択アイテムを新しく重なったアイテムに更新
+    selectionItem.onOverlapDialog() //新しく選択されたアイテムに対して、そのアイテムの重なりダイアログを表示
   }
 
+  //Tiled マップエディタから取得したオブジェクトを Phaser のゲーム内に追加するためのメソッド
   private addObjectFromTiled(
     group: Phaser.Physics.Arcade.StaticGroup,
     object: Phaser.Types.Tilemaps.TiledObject,
     key: string,
     tilesetName: string
   ) {
+    //Tiled で設定された位置（object.x と object.y）を基に、ゲーム内での正しい位置を計算
     const actualX = object.x! + object.width! * 0.5
     const actualY = object.y! - object.height! * 0.5
+    //group.get() メソッドを使用して、指定された位置（actualX, actualY）に新しいオブジェクトを取得
+    //key はオブジェクトのテクスチャを指定し、gid はタイルセットの最初の ID からの相対的な ID を指定
     const obj = group
       .get(actualX, actualY, key, object.gid! - this.map.getTileset(tilesetName).firstgid)
-      .setDepth(actualY)
-    return obj
+      .setDepth(actualY)//オブジェクトの深度を設定
+    return obj//作成したオブジェクトを返す
   }
 
+  //Tiledマップエディタで定義されたオブジェクトのレイヤーを Phaser のゲームシーンに追加,必要に応じて衝突設定を行う
   private addGroupFromTiled(
     objectLayerName: string,
     key: string,
     tilesetName: string,
-    collidable: boolean
+    collidable: boolean //プレイヤーとの衝突設定を行うかどうかを決定するフラグ  true の場合、プレイヤーとの衝突が設定(boolean:真か偽の値をとる)
   ) {
-    const group = this.physics.add.staticGroup()
+    //Tiledオブジェクトの追加
+    const group = this.physics.add.staticGroup()//staticGroup を作成
     const objectLayer = this.map.getObjectLayer(objectLayerName)
     objectLayer.objects.forEach((object) => {
       const actualX = object.x! + object.width! * 0.5
@@ -217,8 +243,10 @@ export default class Game extends Phaser.Scene {
         .get(actualX, actualY, key, object.gid! - this.map.getTileset(tilesetName).firstgid)
         .setDepth(actualY)
     })
-    if (this.myPlayer && collidable)
+    if (this.myPlayer && collidable){ //this.myPlayer が存在し、かつ collidable が真である場合
+      //プレイヤーと作成したグループの衝突を設定
       this.physics.add.collider([this.myPlayer, this.myPlayer.playerContainer], group)
+    }
   }
 
   // function to add new player to the otherPlayer group
@@ -252,10 +280,12 @@ export default class Game extends Phaser.Scene {
     otherPlayer?.updateOtherPlayer(field, value)
   }
 
+  //2つのプレイヤーが重なったときに実行される処理
   private handlePlayersOverlap(myPlayer, otherPlayer) {
     otherPlayer.makeCall(myPlayer, this.network?.webRTC)
   }
 
+  //アイテムにユーザーが追加された際に実行される処理
   private handleItemUserAdded(playerId: string, itemId: string, itemType: ItemType) {
     if (itemType === ItemType.COMPUTER) {
       const computer = this.computerMap.get(itemId)
@@ -266,6 +296,7 @@ export default class Game extends Phaser.Scene {
     }
   }
 
+  //アイテムからユーザーが削除された際に実行される処理
   private handleItemUserRemoved(playerId: string, itemId: string, itemType: ItemType) {
     if (itemType === ItemType.COMPUTER) {
       const computer = this.computerMap.get(itemId)
@@ -276,11 +307,13 @@ export default class Game extends Phaser.Scene {
     }
   }
 
+  //チャットメッセージが追加された際に実行される処理
   private handleChatMessageAdded(playerId: string, content: string) {
     const otherPlayer = this.otherPlayerMap.get(playerId)
     otherPlayer?.updateDialogBubble(content)
   }
 
+  //毎フレーム呼び出されゲームの状態を更新
   update(t: number, dt: number) {
     if (this.myPlayer && this.network) {
       this.playerSelector.update(this.myPlayer, this.cursors)
